19faa571d2a77c948f598b40289a7efcb5cc0ec4
heroksiddiqi <heroksiddiqi@gmail.com>
2022-04-07 02:21:59 +0600

    Added 2 new classes in Data folder: 1. LeaveType.cs 2. LeaveAllocation.cs For Modeling Database

---
+++ LeaveManagementApp/Data/LeaveAllocation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementApp.Data
+{
+    public class LeaveAllocation
+    {
+        public int Id { get; set; } //we have Id Column
+        public int NumberOfDays { get; set; } // It has Number of Days
+        [ForeignKey("LeaveTypeId")] 
+        //FKey pointing to the LeaveType for which, this allocation is being made
+        public LeaveType LeaveType { get; set; } 
+        public int LeaveTypeId { get; set; } 
+        // We have the employee to whom the allocation is being made
+        public string EmployeeId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
