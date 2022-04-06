ef8f02fe2333430e33fe2e400be15424fcd81968
heroksiddiqi <heroksiddiqi@gmail.com>
2022-04-07 03:05:49 +0600

    DRY(Don't Repeat Yourself) Added "BaseEntity.cs" in Data folder because we have some common objects for "LeaveAllocations.cs" & "LeaveType.cs"... Instead of writing same object evertime, we will inherit the base class instead

--- LeaveManagementApp/Data/LeaveAllocation.cs
+++ LeaveManagementApp/Data/LeaveAllocation.cs
@@ -2,9 +2,8 @@
 
 namespace LeaveManagementApp.Data
 {
-    public class LeaveAllocation
+    public class LeaveAllocation: BaseEntity
     {
-        public int Id { get; set; } //we have Id Column
         public int NumberOfDays { get; set; } // It has Number of Days
         [ForeignKey("LeaveTypeId")] 
         //FKey pointing to the LeaveType for which, this allocation is being made
@@ -12,7 +11,5 @@ namespace LeaveManagementApp.Data
         public int LeaveTypeId { get; set; } 
         // We have the employee to whom the allocation is being made
         public string EmployeeId { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
     }
 }
