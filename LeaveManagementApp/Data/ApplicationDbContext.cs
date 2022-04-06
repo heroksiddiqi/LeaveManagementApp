3c3ae41c5ddeae06429deb49454f115b6bd9cb46
heroksiddiqi <heroksiddiqi@gmail.com>
2022-04-07 02:41:38 +0600

    Added Dbset<T> inside DbCOntext Class

--- LeaveManagementApp/Data/ApplicationDbContext.cs
+++ LeaveManagementApp/Data/ApplicationDbContext.cs
@@ -9,5 +9,12 @@ namespace LeaveManagementApp.Data
             : base(options)
         {
         }
+        /*By setting up DbSet, it creates a table modelled off the respective
+        class_name which is set inside DbSet<T> (When Migrated), Here:
+        - Dbset<LeaveType> creates table "LeaveTypes" based on "LeaveType.cs"
+        - Dbset<LeaveAllocation> creates table "LeaveAllocations" based on "LeaveAllocation.cs"
+         */
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }
 }
