a9a61eda343c209284aa3975b3f7c6f953dad83a
heroksiddiqi <heroksiddiqi@gmail.com>
2022-04-14 02:46:49 +0600

    We will use this class as ViewModel to show different labels to users

---
+++ LeaveManagementApp/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementApp.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
