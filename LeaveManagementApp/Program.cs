f233c289e0da3a55967452e8850dedde42243c6f
heroksiddiqi <heroksiddiqi@gmail.com>
2022-04-26 01:54:31 +0600

    -Scaffolded Login and Logout page for Identity Module - Updated Nuget packages - Found the same code-block everytime being generated while scaffolding Identity Module and commented that out for future reference

--- LeaveManagementApp/Program.cs
+++ LeaveManagementApp/Program.cs
@@ -6,6 +6,18 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+/* 
+These lines were being repeatedly generated everytime I scaffolded an Identity modlue whether it's login page or registration page.
+I also tried updating packages from Nuget, but it didn't work. So, i had top manually comment them out for future references
+
+    //var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");; //
+
+    //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    //    options.UseSqlServer(connectionString));; //
+
+    //builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
+    //    .AddEntityFrameworkStores<ApplicationDbContext>();; //
+*/
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
